2f3138abfb385521de2e777180fe9ffeb8909e74
DelmoteAd <91943852+DelmoteAd@users.noreply.github.com>
2022-03-19 10:42:30 +0100

    fin

--- app/src/main/java/com/example/androgreenstudwood/Regions.java
+++ app/src/main/java/com/example/androgreenstudwood/Regions.java
@@ -1,10 +1,15 @@
 package com.example.androgreenstudwood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -43,6 +48,41 @@ public class Regions extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflaterMenu = getMenuInflater();
+        inflaterMenu.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.home:
+                intent = new Intent(Regions.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.ville:
+                intent = new Intent(Regions.this, Region.class);
+                startActivity(intent);
+                break;
+            case R.id.qcm:
+                intent = new Intent(Regions.this, QCM.class);
+                startActivity(intent);
+                break;
+            case R.id.apropos:
+                intent = new Intent(Regions.this, A_Propos.class);
+                startActivity(intent);
+                break;
+            case R.id.quitter:
+                System.exit(0);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
     @Override
     protected void onDestroy() {
